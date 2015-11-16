df2c70ca4d4ae7659584073ab64ba8628cfde554
Aaron Zakem <Aaron Zakem>
2015-11-15 21:42:50 -0500

    createDataset.py stores records to litesql database

--- createDataset.py
+++ createDataset.py
@@ -1,24 +1,30 @@
 from citemachine.corpus.dblp import DBLP
+import sqlite3 as lite
+import sys
 
 #set to the location where dblp is stored
 path_to_dblp = 'acm_output.txt'
-#set to location for output dataset
-path_to_output = 'dataset.txt'
+#set to location for output sqlite database
+path_to_output = 'dblpextract.db'
 #set to a small value like 5000 to test on a subset of the data
 num_docs = 5000
 
 #parses the dataset
 dblp = DBLP(path_to_dblp, max_docs=num_docs)
 
-#iterate through documents and write attributes to output file
-with open(path_to_output, 'w') as document:
+#iterate through documents and write attributes to output database
+with lite.connect(path_to_output) as con:
+    cur = con.cursor();
+    cur.execute("DROP TABLE IF EXISTS Articles")
+    cur.execute("CREATE TABLE Articles(ID INT, Title TEXT, Author TEXT, \
+    Year TEXT, Conference TEXT, Cite_Count TEXT, Refs TEXT, Abstract TEXT)")
     for key in dblp.keys():
-        output = ""
-        output += str(dblp.titles[key]) + ","
-        output += str(dblp.authors[key]) + ","
-        output += str(dblp.years[key]) + ","
-        output += str(dblp.conferences[key]) + ","
-        output += str(dblp.citation_counts[key]) + ","
-        output += str(dblp.references[key]) + ","
-        output += str(dblp.abstracts[key]) + "\n"
-        document.write(output)
+        title = str(dblp.titles[key]) + ","
+        author = str(dblp.authors[key]) + ","
+        year = str(dblp.years[key]) + ","
+        conference = str(dblp.conferences[key]) + ","
+        cite_count = str(dblp.citation_counts[key]) + ","
+        refs = str(dblp.references[key]) + ","
+        abstract = str(dblp.abstracts[key]) + "\n"
+        cur.execute("INSERT INTO Articles VALUES(?,?,?,?,?,?,?,?)", (key, \
+        title, author, year, conference, cite_count, refs, abstract))
